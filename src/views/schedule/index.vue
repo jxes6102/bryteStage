63895fc9e04f727a5b475efd60efde7195f2bc4c
jxes6102 <jxes6102@gamil.com>
2024-02-01 17:51:51 +0800

    日程管理 - 2

--- src/views/schedule/index.vue
+++ src/views/schedule/index.vue
@@ -6,32 +6,38 @@
             日程管理
         </div>
         <div class="line-style w-[100%] text-[#D3D3D3] flex"></div>
-        <el-calendar 
-            ref="calendar" 
-            class="w-[95vw] md:w-[75vw] flex flex-wrap justify-center md:justify-start items-center">
-            <template #header="{ date }">
-                <div class="flex flex-col justify-center items-center">
-                    <!-- <span>Custom header content</span> -->
-                    <span class="text-2xl">{{ date }}</span>
-                    <el-button-group>
-                        <!-- <el-button size="small" @click="selectDate('prev-year')">
-                            上一年
-                        </el-button> -->
-                        <el-button style="font-size:16px;" size="small" @click="selectDate('prev-month')">
-                            上一月
-                        </el-button>
-                        <el-button style="font-size:16px;" size="small" @click="selectDate('today')">今天</el-button>
-                        <el-button style="font-size:16px;" size="small" @click="selectDate('next-month')">
-                            下一月
-                        </el-button>
-                        <!-- <el-button size="small" @click="selectDate('next-year')">
-                            下一年
-                        </el-button> -->
-                    </el-button-group>
-                </div>
-                
-            </template>
-        </el-calendar>
+        <div class="w-full h-full">
+            <el-calendar 
+                ref="calendar" 
+                class="w-[95vw] md:w-[75vw] flex flex-wrap justify-center md:justify-start items-center">
+                <template #header="{ date }">
+                    <div class="flex flex-col justify-center items-center">
+                        <span class="text-2xl">{{ date }}</span>
+                        <el-button-group>
+                            <!-- <el-button size="small" @click="selectDate('prev-year')">
+                                上一年
+                            </el-button> -->
+                            <el-button style="font-size:16px;" size="small" @click="selectDate('prev-month')">
+                                上個月
+                            </el-button>
+                            <el-button style="font-size:16px;" size="small" @click="selectDate('today')">今天</el-button>
+                            <el-button style="font-size:16px;" size="small" @click="selectDate('next-month')">
+                                下個月
+                            </el-button>
+                            <!-- <el-button size="small" @click="selectDate('next-year')">
+                                下一年
+                            </el-button> -->
+                        </el-button-group>
+                    </div>
+                </template>
+                <template #date-cell="item">
+                    <div @click="test(item.data)" class="w-full h-full flex flex-wrap justify-start items-start">
+                        {{ item.data.day }}
+                    </div>
+                </template>
+            </el-calendar>
+        </div>
+        
     </div>
 </template>
 
@@ -41,7 +47,7 @@ import { ref,computed } from "vue"
 import { useRouter,useRoute } from "vue-router"
 import { useMobileStore } from '@/stores/index'
 // import { getRoleList,getUserList,setUserEdit } from '@/api/api'
-import dialogView from "@/components/dialogView.vue"
+// import dialogView from "@/components/dialogView.vue"
 
 const router = useRouter()
 const route = useRoute()
@@ -56,9 +62,14 @@ const selectDate = (val) => {
     if (!calendar.value){
         return false
     }
-    console.log('val',val)
-    console.log('calendar.value',calendar.value)
+    // console.log('val',val)
+    // console.log('calendar.value',calendar.value)
     calendar.value.selectDate(val)
+}
+
+const test = (item) => {
+    console.log('============')
+    console.log('item',item)
 }
 
 
@@ -72,4 +83,20 @@ init()
 
 <style lang="scss" scoped>
 
+@media screen and (min-width: 768px) {
+    :deep(.el-calendar-table .el-calendar-day ){
+        height: 100px !important;
+    }
+}
+
+@media screen and (min-width: 1500px) {
+    :deep(.el-calendar-table .el-calendar-day ){
+        height: 150px !important;
+    }
+}
+
+
+:deep(.el-calendar-table__row):nth-child(even){
+    background-color:rgb(249 250 251);
+}
 </style>
